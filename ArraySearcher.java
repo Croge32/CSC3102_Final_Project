74ccbf5f16ab13a8dfad7bc350a97beca99454c0
Cody Rogers <croge32@lsu.edu>
2012-11-29 15:00:21 -0600

    updating arraysearcher

--- ArraySearcher.java
+++ ArraySearcher.java
@@ -1,17 +1,17 @@
 package PrefixSearch;
 
-public class ArraySearcher implements PrefixSearchable{
-	
-	String[] substringArray;
-	
-	public String[] findSubstrings(String prefix){
-		// TODO Use precompiled parent to search out substrings
-		
-		return null;
-	}
-	
-	public void precompile(String parent){
-		
-	}
+public abstract class ArraySearcher implements PrefixSearchable{
+
+private String[] substrings;
+
+    public String[] findSubstrings(String prefix){
+        return null;
+    }
+
+    public void precompile(String parent){
+        for(int i=0; i<parent.length(); i++){
+            substrings[i] = parent.substring(i);
+        }
+    }
 	
 }
