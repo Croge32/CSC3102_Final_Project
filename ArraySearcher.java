66eacb829e9ad9d0feef3738e306db74a9810d7e
Cody Rogers <croge32@lsu.edu>
2012-11-29 16:49:01 -0600

    Binary search completed

--- ArraySearcher.java
+++ ArraySearcher.java
@@ -2,11 +2,12 @@ package PrefixSearch;
 
 import java.util.Arrays;
 
-public class ArraySearcher implements comparable, PrefixSearchable{
+public class ArraySearcher implements PrefixSearchable{
 
 private String[] substrings;
+private int parentLength;
 
-    public String[] findSubstrings(String prefix){      
+    public String[] findSubstrings(String prefix){ 
         
         return null;
     }
@@ -16,6 +17,19 @@ private String[] substrings;
             substrings[i] = parent.substring(i);
         }
         Arrays.sort(substrings);
+    }
+    
+    public int binarySearch(String[] s, String key){
+        int low = 0;
+        int high = s.length - 1;
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if (key.compareTo(s[mid]) == -1) {high = mid - 1;}
+            else if (key.compareTo(s[mid]) == 1) {low = mid + 1;}
+            else return mid;
+        }
+        return -1;
     }
     
 }
