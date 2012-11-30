84ed46620b3caba104ad6d5305c0a5d7ebee899b
Cody Rogers <croge32@lsu.edu>
2012-11-29 20:39:55 -0600

    Main method testing

--- PrefixSearcher.java
+++ PrefixSearcher.java
@@ -3,7 +3,14 @@ package PrefixSearch;
 public class PrefixSearcher {
     
     public static void main(String[] args){
-        // TODO Handle input - output
+        ArraySearcher a = new ArraySearcher();
+        String s = "string";
+        String[] sArray = new String[s.length()];
+        a.precompile(s);
+        sArray = a.substrings;
+        for(int i=0; i<sArray.length; i++){
+            System.out.println(sArray[i]);
+        }
     }
     
 }
